824f152fe3f9621e3df820d56a2c15dcd5885192
hugo oliveira <hugooliveirahenrique@gmail.com>
2020-02-03 21:05:15 -0200

    creating model controller

---
+++ src/app/models/User.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-param-reassign */
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcrypt';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // This will never be inject in database
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash('user.password', 8); // The eight is the strength of the hash
+      }
+    }); // There a functions execute depending of some actions in sequelize
+    return this;
+  }
+}
+
+export default User;
