1374547c06b6a1fb1a6cde231919e4169bcc5250
hugo oliveira <hugooliveirahenrique@gmail.com>
2020-02-14 22:47:56 -0200

    validando token

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,21 +17,21 @@ class UserController {
 
   async update(req, res) {
     const { email, oldPassword } = req.body;
-
     const user = await User.findByPk(req.userId);
 
-    if (email !== user.email) {
+    if (email && email !== user.email) {
       const userExists = await User.findOne({
         where: { email: req.body.email },
       });
       if (userExists) {
         return res.status(400).json({ error: 'User already exists' });
       }
-      if (oldPassword && !(await user.checkPassword(oldPassword))) {
-        return res.status(401).json({ error: 'Password does not match' });
-      }
     }
-    return res.json({ ok: true });
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+    const { id, name, provider } = await user.update(req.body);
+    return res.json({ id, name, email, provider });
   }
 }
 
