37b13e0b9e45a11e39f13dff4787ab5d9f38d71b
hugo oliveira <hugooliveirahenrique@gmail.com>
2020-02-14 22:19:08 -0200

    creating session controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -14,6 +14,25 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email: req.body.email },
+      });
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+      if (oldPassword && !(await user.checkPassword(oldPassword))) {
+        return res.status(401).json({ error: 'Password does not match' });
+      }
+    }
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
