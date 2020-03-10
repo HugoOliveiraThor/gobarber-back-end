74c79df0464d23ec817e18d33403a22913c6a571
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2020-03-10 20:35:02 -0300

    new appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,16 +1,21 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/Files';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
     const appointments = await Appointment.findAll({
       where: {
         user_id: req.userId,
         canceled_at: null,
       },
+      limit: 20, // Limit of page
+      offset: (page - 1) * 20, // If a need jump any register
       order: ['date'],
       attributes: ['id', 'date'],
       include: [
@@ -80,11 +85,24 @@ class AppointmentController {
         .status(400)
         .json({ error: 'Appointment date is not avaiable' });
     }
+    if (req.userId === provider_id)
+      return res.status(400).json({ error: 'You cannot mark to yourself' });
 
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    // Notify provider
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(hourStart, "'dia' dd 'de' MMMM',ás' H:mm'h'", {
+      locale: pt,
+    });
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
